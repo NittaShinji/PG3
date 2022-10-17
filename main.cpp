e9059add754fddd24ac12d204aeedc5d77742e91
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2022-10-17 17:34:11 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -4,25 +4,23 @@
 #include <time.h>
 #include <functional>
 
-typedef void (*PFunc)(int*, int*, int*);
-
 //コールバック関数
-void CallBack1(int *s, int *correct, int *playerNum)
+void CallBack(int correct, int playerNum)
 {
 	//偶数
-	if (*playerNum % 2 == *correct)
+	if (playerNum % 2 == correct)
 	{
 		printf("当たりです\n");
 	}
 	//奇数
-	else if (*playerNum % 2 != *correct)
+	else if (playerNum % 2 != correct)
 	{
 		printf("外れです\n");
-		if (*correct == 0)
+		if (correct == 0)
 		{
 			printf("ダイスは偶数\n");
 		}
-		if (*correct == 1)
+		if (correct == 1)
 		{
 			printf("ダイスは奇数\n");
 		}
@@ -30,36 +28,27 @@ void CallBack1(int *s, int *correct, int *playerNum)
 	else {}
 }
 
-void SetTimeOut(PFunc p, int second, int correct, int playerNum)
-{
-	//何秒間待つ
-	Sleep(second * 1000);
-	//コールバック関数を呼び出す
-	p(&second, &correct, &playerNum);
-}
-
 int main(int argc, const char *argv[])
 {
-	//関数ポインタ
-	PFunc p;
-	p = CallBack1;
-
 	//サイコロの値
 	srand(time(nullptr));
 	int diceNum = rand() % 6 + 1;
 
 	//ラムダ式
-	//正解の値の判別(偶数か奇数か)
-	std::function<int(void)> correct = [=]{return diceNum % 2; };
-
+	//乱数から値の判別(偶数か奇数か)
+	auto correct = [=]{return diceNum % 2; };
+	
 	printf("偶数か奇数を入力してください\n");
 	//プレイヤー入力
 	int playerNum = 0;
 	scanf_s("%d", &playerNum);
 
 	printf("抽選スタート\n");
-	//三秒待って結果を表示
-	SetTimeOut(p, 3, correct(), playerNum);
+	//三秒待つ
+	std::function<void(int)> setSleep = [](int second) {Sleep(second * 1000); };
+	setSleep(3);
+	//結果を表示
+	CallBack(correct(), playerNum);
 
 	return 0;
 }
