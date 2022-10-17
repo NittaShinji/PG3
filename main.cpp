db8aa359ebd759e84f67950729c0a356e4c68a4b
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2022-10-17 11:30:56 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,29 +1,70 @@
-#include"stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <windows.h>
+#include <time.h>
 
-//関数テンプレート
-template<typename Type>
-Type Min(Type a, Type b)
+typedef void (*PFunc)(int *);
+
+//コールバック関数
+void CallBack1(int *s)
 {
-	if (a >= b)
-	{
-		return static_cast<Type>(b);
-	}
-	return static_cast<Type>(a);
+	printf("%d秒後に実行されるよ\n", *s);
 }
 
-//テンプレートの特殊化(オーバーライド)
-template<>
-char Min<char>(char a, char b)
+//何秒後に呼び出す関数
+void SetTimeOut(PFunc p,int second)
 {
-	return printf("数字以外は代入できません");
+	//コールバック関数を呼び出す
+	Sleep(second);
+}
+
+//正解を出力する関数
+void ShowCorrect(int correct)
+{
+	if (correct == 0)
+	{
+		printf("ダイスは偶数\n");
+	}
+	if (correct == 1)
+	{
+		printf("ダイスは奇数\n");
+	}
 }
 
 int main()
 {
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(114, 514));
-	printf("%lf\n",Min<double>(114, 514));
-	printf("%c\n", Min<char>(114, 514));
+	//プレイヤー入力用の変数
+	int playerNum = 0;
+	scanf_s("%d",&playerNum);
+
+	//サイコロの値
+	srand(time(nullptr));
+	int diceNum = rand() % 6 + 1;
+
+	//正解の値の判別(偶数か奇数か)
+	int correct = diceNum % 2;
+	
+	//関数ポインタ
+	PFunc p;
+	p = CallBack1;
+
+	printf("抽選スタート\n");
+	//＜入力受付＞
+	//偶数
+	if (playerNum % 2 == correct)
+	{
+		SetTimeOut(p, 3000);
+		printf("当たりです\n");
+	}
+	//奇数
+	else if (playerNum % 2 != correct)
+	{
+		SetTimeOut(p, 3000);
+		printf("外れです\n");
+		ShowCorrect(correct);
+	}
+	else {}
 
 	return 0;
 }
