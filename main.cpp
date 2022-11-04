328a43a7ff2871be70b0c8e7efffca2bca6f238e
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2022-11-04 21:00:40 +0900

    2-1　完了

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <list>
 #include <iostream>
 
+//全駅を表示
+void PrintStation(std::list<const char*>& lst);
+
 int main()
 {
 	//駅数(路線記号[JY])
@@ -16,33 +19,33 @@ int main()
 	"Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
 	"Tamachi", "Hamamatsucho","Shimbashi", "Yurakucho"};
 
-	//char stationName2 = { 'a'};
-
 	//元データから双方向リストを生成
-	std::list<const char*> lstJY(stationName, std::end(stationName));
+	std::list<const char*> lst(stationName, std::end(stationName));
 
-	//std::list<char>::iterator itr
+	printf("1970年駅一覧\n");
+	PrintStation(lst);
+	printf("\n");
 
-	//リストにはオペレーターがなく、大きさ比較できないので<>は使えない
-	for (auto itr = lstJY.begin(); itr != lstJY.end(); ++itr)
+	printf("2019年駅一覧\n");
+	std::list<const char*>::iterator itr1971 = std::next(lst.begin(), 7);
+	lst.insert(itr1971, "NishiniNippori");
+	PrintStation(lst);
+	printf("\n");
+
+	printf("2022年駅一覧\n");
+	std::list<const char*>::iterator itr2019 = std::next(lst.begin(), 25);
+	lst.insert(itr2019, "TakanawaGateway");
+	PrintStation(lst);
+	printf("\n");
+
+	return 0;
+}
+
+//全駅を表示
+void PrintStation(std::list<const char*>& lst)
+{
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
 	{
 		std::cout << *itr << "\n";
 	}
-
-	//printf("%s\n", stationName[1]);
-
-
-	
-	/*std::list<int> it = { 1,2,4 };
-	
-	std::list<int>::iterator itr = std::next(it.begin(), 2);
-	it.insert(itr, 3);
-	
-	for (auto itr = it.begin(); itr != it.end(); itr++)
-	{
-		std::cout << *itr << "\n";
-	}*/
-
-
-	return 0;
 }
